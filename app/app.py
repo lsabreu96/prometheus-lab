5c51ad4b4d65a8b8c5d6d756ebaa374c8108fe5d
Lucas Abreu <sa.lucas96@gmail.com>
2025-01-03 16:50:07 -0300

    refac(app): remove prometheus client

--- app/app.py
+++ app/app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
 
 @app.route('/')
 def hello():
     return "Olá"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
